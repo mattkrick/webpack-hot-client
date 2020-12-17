b61fc321729d88b208c08ec1d78e4e192efa509a
Matt Krick <matt.krick@gmail.com>
2020-12-17 08:44:26 -0800

    remove loglevelnext

--- client/log.js
+++ client/log.js
@@ -1,70 +1,83 @@
 "use strict";
 
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+var levels = ['trace', 'debug', 'info', 'warn', 'error', 'silent'];
 
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+var Log =
+/*#__PURE__*/
+function () {
+  function Log() {
+    _classCallCheck(this, Log);
 
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+    this.level = 'error';
+  }
 
-function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+  _createClass(Log, [{
+    key: "group",
+    value: function group() {
+      var _console;
 
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
-// eslint-disable-next-line import/no-extraneous-dependencies
-var loglevel = require('loglevelnext/dist/loglevelnext');
-
-var MethodFactory = loglevel.factories.MethodFactory;
-var css = {
-  prefix: 'color: #999; padding: 0 0 0 20px; line-height: 16px; background: url(https://webpack.js.org/6bc5d8cf78d442a984e70195db059b69.svg) no-repeat; background-size: 16px 16px; background-position: 0 -2px;',
-  reset: 'color: #444'
-};
-var log = loglevel.getLogger({
-  name: 'hot',
-  id: 'hot-middleware/client'
-});
-
-function IconFactory(logger) {
-  MethodFactory.call(this, logger);
-}
-
-IconFactory.prototype = Object.create(MethodFactory.prototype);
-IconFactory.prototype.constructor = IconFactory;
-
-IconFactory.prototype.make = function make(methodName) {
-  var og = MethodFactory.prototype.make.call(this, methodName);
-  return function _() {
-    for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
-      params[_key] = arguments[_key];
+      (_console = console).group.apply(_console, arguments);
     }
+  }, {
+    key: "groupCollapsed",
+    value: function groupCollapsed() {
+      var _console2;
 
-    var args = [].concat(params);
-    var prefix = '%c｢hot｣ %c';
-
-    var _args = _slicedToArray(args, 1),
-        first = _args[0];
-
-    if (typeof first === 'string') {
-      args[0] = prefix + first;
-    } else {
-      args.unshift(prefix);
+      (_console2 = console).groupCollapsed.apply(_console2, arguments);
     }
+  }, {
+    key: "groupEnd",
+    value: function groupEnd() {
+      var _console3;
 
-    args.splice(1, 0, css.prefix, css.reset);
-    og.apply(void 0, _toConsumableArray(args));
-  };
-};
+      (_console3 = console).groupEnd.apply(_console3, arguments);
+    }
+  }, {
+    key: "trace",
+    value: function trace() {
+      var _console4;
 
-log.factory = new IconFactory(log, {});
-log.group = console.group; // eslint-disable-line no-console
+      if (levels.indexOf(this.level) > levels.indexOf('trace')) return;
 
-log.groupCollapsed = console.groupCollapsed; // eslint-disable-line no-console
+      (_console4 = console).trace.apply(_console4, arguments);
+    }
+  }, {
+    key: "info",
+    value: function info() {
+      var _console5;
 
-log.groupEnd = console.groupEnd; // eslint-disable-line no-console
+      if (levels.indexOf(this.level) > levels.indexOf('info')) return;
 
+      (_console5 = console).info.apply(_console5, arguments);
+    }
+  }, {
+    key: "warn",
+    value: function warn() {
+      var _console6;
+
+      if (levels.indexOf(this.level) > levels.indexOf('warn')) return;
+
+      (_console6 = console).warn.apply(_console6, arguments);
+    }
+  }, {
+    key: "error",
+    value: function error() {
+      var _console7;
+
+      if (levels.indexOf(this.level) > levels.indexOf('error')) return;
+
+      (_console7 = console).error.apply(_console7, arguments);
+    }
+  }]);
+
+  return Log;
+}();
+
+var log = new Log();
 module.exports = log;
